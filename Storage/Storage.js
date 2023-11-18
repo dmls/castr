8e73f3ce6f3d4647ab82319549cccb11e318b11e
dmls <danmlsv@gmail.com>
2023-11-18 16:10:43 -0500

    Update doc comment

--- Storage/Storage.js
+++ Storage/Storage.js
@@ -2,12 +2,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 /**
  * Retrieves the 'collections' record from AsyncStorage.
- *
- * @async
- * @function
  * @returns {Promise<Array|boolean>} - A promise that resolves to an array of collections if successful,
  *                                     or `false` if an error occurs.
- * @throws {Error} - Throws an error if there is an issue loading collections from AsyncStorage.
  */
 const getCollections = async () => {
   try {
