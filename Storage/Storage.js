2fb8f32dd84ed1814db63637726495b7a75ec986
dmls <danmlsv@gmail.com>
2023-11-18 15:41:29 -0500

    Add doc comment for getCollections method
    Update return type to boolean

--- Storage/Storage.js
+++ Storage/Storage.js
@@ -1,12 +1,21 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+/**
+ * Retrieves the 'collections' record from AsyncStorage.
+ *
+ * @async
+ * @function
+ * @returns {Promise<Array|boolean>} - A promise that resolves to an array of collections if successful,
+ *                                     or `false` if an error occurs.
+ * @throws {Error} - Throws an error if there is an issue loading collections from AsyncStorage.
+ */
 const getCollections = async () => {
   try {
     const collectionsString = await AsyncStorage.getItem('collections');
     return collectionsString ? JSON.parse(collectionsString) : [];
   } catch (error) {
     console.error('Error loading collections from AsyncStorage:', error);
-    return null;
+    return false;
   }
 };
 
