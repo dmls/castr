71d954ebd232c8a07878ad90011a2ea1569c3173
dmls <danmlsv@gmail.com>
2023-11-18 15:42:03 -0500

    Add method to delete collections by name

--- Storage/Storage.js
+++ Storage/Storage.js
@@ -20,4 +20,32 @@ const getCollections = async () => {
   }
 };
 
-export { getCollections };
+/**
+ * Delete collections with the specified names.
+ * @param array {string[]} names - An array of strings representing the names of collections to delete.
+ * @returns boolean {boolean} - Returns true on success, false on error.
+ */
+const deleteCollections = async (names) => {
+  try {
+    const resultString = await AsyncStorage.getItem('collections');
+    let resultArray = JSON.parse(resultString || '[]');
+
+    names.forEach((name) => {
+      const indexToDelete = resultArray.findIndex((item) => item.name === name);
+
+      if (indexToDelete !== -1) {
+        resultArray.splice(indexToDelete, 1);
+      }
+    });
+
+    await AsyncStorage.setItem('collections', JSON.stringify(resultArray));
+
+    return true;
+  } catch (error) {
+    console.error('Error deleting collection:', error);
+    return false;
+  }
+};
+
+
+export { getCollections, deleteCollections };
