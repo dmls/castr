76aabb98db2659a8db8dbbd5164fa0528dcc6d37
dmls <danmlsv@gmail.com>
2023-12-07 22:32:17 -0500

    Add confirmation dialogue to delete button

--- Components/DeleteCollectionButton.js
+++ Components/DeleteCollectionButton.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Alert } from 'react-native';
 import { styles } from '../assets/styles/Styles';
 import { deleteCollections } from '../Storage/Storage';
 
 const DeleteCollectionButton = ({ collection, navigation }) => {
+  const handlePress = () => {
+    Alert.alert(
+      'Confirm Deletion',
+      `Are you sure you want to delete ${collection.name}?`,
+      [
+        { text: 'Cancel' },
+        { text: 'Delete', onPress: async () => {
+          const del = await deleteCollections([collection.name]);
+          navigation.push('Collections');
+        } },
+      ]
+    );
+  };
+
   return (
     <TouchableOpacity
       style={[styles.button, styles.buttonDanger]}
       activeOpacity={0.5}
-      onPress={() => deleteCollections([collection.name]) && navigation.push('Collections')}
+      onPress={handlePress}
     >
       <Text style={styles.buttonText}>Delete</Text>
     </TouchableOpacity>
