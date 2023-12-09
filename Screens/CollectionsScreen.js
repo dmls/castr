8c16a9eb44a3c01730f18d3314d24fc527a3dae3
dmls <danmlsv@gmail.com>
2023-12-08 23:31:05 -0500

    Remove back button from home/Collections screen

--- Screens/CollectionsScreen.js
+++ Screens/CollectionsScreen.js
@@ -1,9 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import { View, Text, TouchableOpacity, Touchable } from 'react-native';
 import { styles } from '../assets/styles/Styles';
 import { getCollections } from '../Storage/Storage';
 
 const CollectionsScreen = ({ navigation }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerBackVisible: false,
+    });
+  }, [navigation]);
+
   const [collections, setCollections] = useState([]);
 
   useEffect(() => {
