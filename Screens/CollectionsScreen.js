03d4c2abb1401920faf6ce09079d190f8b31d73f
dmls <danmlsv@gmail.com>
2023-12-23 23:58:37 -0500

    Fix scrolling on Collections screen

--- Screens/CollectionsScreen.js
+++ Screens/CollectionsScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useLayoutEffect } from 'react';
-import { View, Text, TouchableOpacity, Touchable } from 'react-native';
+import { ScrollView, View, Text, TouchableOpacity, Touchable } from 'react-native';
 import { styles } from '../assets/styles/Styles';
 import { getCollections } from '../Storage/Storage';
 
@@ -22,7 +22,7 @@ const CollectionsScreen = ({ navigation }) => {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={styles.section}>
         <View style={styles.sectionRow}>
           <Text>{JSON.stringify(collections)}</Text>
@@ -48,7 +48,7 @@ const CollectionsScreen = ({ navigation }) => {
           <Text style={styles.buttonText}>Create a collection</Text>
         </TouchableOpacity>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
