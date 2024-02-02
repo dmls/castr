3c4d213dae75780b3ff83e706e57b97d0e179094
dmls <danmlsv@gmail.com>
2024-02-01 21:00:09 -0500

    Add full width image card for collections screen

--- Components/CardThumbnail.js
+++ Components/CardThumbnail.js
@@ -3,24 +3,44 @@ import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { styles } from '../assets/styles/Styles';
 import { print } from '../Utils/Debug';
 
-const CardThumbnail = ({ index, onPress, data, actions }) => {
+const CardThumbnail = ({ index, onPress, data, actions, fullWidth = false }) => {
+  const CardTitle = (
+    <View style={{flex: 2, flexDirection: 'column', justifyContent: 'space-between'}}>
+      <View>
+        <Text style={styles.h2}>{data.name}</Text>
+      </View>
+
+      {!fullWidth && (<View>{actions}</View>)}
+    </View>
+  );
+
+  const CardImage = (
+    <View style={{ flex: 1, ...(fullWidth ? {} : { height: 100 }) }}>
+      <Image source={{uri: data.image}} style={[styles.imageFullWidth]} />
+    </View>
+  );
+  
   return (
     <TouchableOpacity key={index} onPress={onPress} style={styles.section}>
-      <View style={styles.sectionRow}>
-        <View style={{flex: 2, flexDirection: 'column', justifyContent: 'space-between'}}>
-          <View>
-            <Text style={styles.h2}>{data.name}</Text>
+
+      {fullWidth && (
+        <>
+          <View style={styles.sectionRow}>
+            {CardTitle}
           </View>
 
-          {actions}
-        </View>
+          {CardImage}
+        </>
+      )}
 
-        <View style={{flex: 1, height: 100}}>
-          <Image source={{uri: data.image}} style={[styles.imageFullWidth]} />
+      {!fullWidth && (
+        <View style={styles.sectionRow}>
+          {CardTitle}
+          {CardImage}
         </View>
-      </View>
+      )}
       
-      {print(data)}
+      {/* {print(data)} */}
     </TouchableOpacity>  
   );
 }
