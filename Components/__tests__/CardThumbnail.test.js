23fadc390ec0f723f1aea99eada948c177f3126b
dmls <danmlsv@gmail.com>
2024-02-24 15:00:19 -0500

    Add test CardThumbnail

---
+++ Components/__tests__/CardThumbnail.test.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Text } from 'react-native';
+import renderer from 'react-test-renderer';
+
+import CardThumbnail from '../CardThumbnail';
+
+const index = 0;
+const onPress = () => { /* onPress callback code */ };
+const data = {
+  name: 'data name',
+  image: 'file://image-path-here.jpg',
+};
+const actions = (
+  <>
+    <Text>Sample action 1</Text>
+    <Text>Sample action 2</Text>
+  </>
+);
+
+describe('CardThumbnail', () => {
+  test('renders correctly with default settings', () => {
+    const component = renderer.create(
+      <CardThumbnail
+        index={index}
+        onPress={onPress}
+        data={data}
+        actions={actions}
+      />
+    );
+    const tree = component.toJSON();
+    
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('renders correctly with fullWidth = false', () => {
+    const component = renderer.create(
+      <CardThumbnail
+        index={index}
+        onPress={onPress}
+        data={data}
+        actions={actions}
+        fullWidth={false}
+      />
+    );
+    const tree = component.toJSON();
+    
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('renders correctly with fullWidth = true', () => {
+    const component = renderer.create(
+      <CardThumbnail
+        index={index}
+        onPress={onPress}
+        data={data}
+        actions={actions}
+        fullWidth={true}
+      />
+    );
+    const tree = component.toJSON();
+    
+    expect(tree).toMatchSnapshot();
+  });
+});
